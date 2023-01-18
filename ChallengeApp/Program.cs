36d4a60f5b08b9a81c4aefa9761e2eb8c2e609c1
cinioxp <marcinpawlus7@mail.com>
2023-01-18 16:29:02 +0000

    Add project files.

---
+++ ChallengeApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+string name = "Viola";
+char sex = 'K';
+int age = 27;
+
+if (sex == 'K' && age < 28)
+{
+    Console.WriteLine("kobieta ponziej 27 Wiosen");
+}
+else if (name == "Viola" && age == 28)
+{
+    Console.WriteLine("Jestem Viola 28 Lat");
+}
+else
+{
+    if (sex == 'M' && age < 18)
+    {
+        Console.WriteLine("niepelnoletni mezczyzna");
+    }
+}
+
+
